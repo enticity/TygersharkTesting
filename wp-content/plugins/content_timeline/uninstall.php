5a0580bad523dd8a26ae3dd82b3acf5961c21c40
fadits <fadi@tygershark.com>
2017-11-28 21:39:20 -0500

    clean install

--- wp-content/plugins/content_timeline/uninstall.php
+++
@@ -1,7 +0,0 @@
-<?php 
-if( !defined( 'ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') )
-	exit();
-
-/*global $wpdb;
-$sliders_table = $wpdb->prefix . 'ctimelines';
-$wpdb->query( "DROP TABLE $sliders_table" );*/
--- wp-content/plugins/fastgallery_vc/assets/js/fg-masonry.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-jQuery(window).load(function() {
-	jQuery(document).ready(function($){
-		$('.fastgallery.brick-masonry').masonry({
-			singleMode: true,
-			itemSelector: '.fg-gallery-item'
-		});
-	});
-});
